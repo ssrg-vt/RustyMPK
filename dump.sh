b6bc423ac29209cd8f73ae4af3d0e552e706926d
mincheol <mincheol@vt.edu>
2020-02-18 19:55:05 +0000

    Unstage script files

--- dump.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-objdump -S -C tests/target/x86_64-unknown-hermit/debug/rusty_tests > dump
--- launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo /home/mincheol/uhyve/uhyve tests/target/x86_64-unknown-hermit/debug/rusty_tests -v
